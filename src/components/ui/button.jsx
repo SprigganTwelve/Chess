07fd52ee2c66fbb96494ad789d165e29cc607441
Paterne <angepaterne.dali@gmail.com>
2024-12-19 15:08:06 +0100

    public images and first page

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line react/prop-types
+const Button = ({text}) => {
+    return ( 
+        <button>
+            {text}
+        </button>
+     );
+}
+ 
+export default Button;
